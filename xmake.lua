609f328d2b28a9215dac89fb46f224a156bc5581
Xingyu <1773362692@qq.com>
2023-02-16 14:54:54 +0800

    update xmake

--- xmake.lua
+++ xmake.lua
@@ -3,7 +3,7 @@ add_rules("mode.debug", "mode.release")
 add_includedirs("source/")
 
 target("main")
-    set_languages("c++20")
+    set_languages("cxx20")
     set_kind("binary") 
     add_files("source/**.cpp")
     add_packages("libsdl", "glm") 
